fc96239bb6fe85b4a90fa04eca5fcc8f5a757afc
Alejandro Bertolet <abertoletreina@mgh.harvard.edu>
2022-08-28 20:51:05 -0400

    Fixing bug in the bootstrapping implementation and adding statistical error bands

--- bootstrapping.py
+++ bootstrapping.py
@@ -12,17 +12,18 @@ import random
 
 from scipy import interpolate
 
-nboot = 15
+nboot = 20
 
 maxdose = 8.0
 Dose = np.linspace(0, maxdose, 100)
 DSB = np.zeros(Dose.shape)
+alldsbs = np.zeros([len(Dose), nboot])
 SSB = np.zeros(Dose.shape)
 BD = np.zeros(Dose.shape)
 ntries = np.zeros(Dose.shape)
-damage = DamageToDNA()
 basepath = '/Users/ai925/Dropbox (Partners HealthCare)/Microdosimetry Project/ChemMicrodosimetry/nucleusSims/proton/sims/1MeV.txt/'
 for i in range(nboot):
+    damage = DamageToDNA()
     neworder = random.sample(range(225), 225)
     for j in neworder:
         path = basepath + str(j) + '/'
@@ -43,12 +44,20 @@ for i in range(nboot):
     fdsb = interpolate.interp1d(dose, dsb)
     fssb = interpolate.interp1d(dose, ssb)
     fbd = interpolate.interp1d(dose, bd)
-    for i, d in enumerate(Dose):
+    for id, d in enumerate(Dose):
         if d >= 0 and d < np.max(dose):
-            DSB[i] += fdsb(d)
-            SSB[i] += fssb(d)
-            BD[i] += fbd(d)
-            ntries[i] += 1
+            DSB[id] += fdsb(d)
+            alldsbs[id, i] = fdsb(d)
+            SSB[id] += fssb(d)
+            BD[id] += fbd(d)
+            ntries[id] += 1
+
+vardsb = np.zeros(Dose.shape)
+for j in range(alldsbs.shape[0]):
+    vardsb[j] = np.var(alldsbs[j,:])
+
+stddsb = np.sqrt(vardsb)
+
 DSB = DSB / ntries
 SSB = DSB / ntries
 BD = BD / ntries
@@ -56,7 +65,8 @@ BD = BD / ntries
 fig = plt.figure()
 fig.set_size_inches((4, 4))
 ax = fig.add_subplot(111)
-ax.plot(Dose, DSB)
+ax.plot(Dose, DSB, 'k-')
+ax.fill_between(Dose, DSB-stddsb, DSB+stddsb)
 ax.set_xlim(0, 1.01*maxdose)
 ax.set_ylim(0, None)
 ax.set_xlabel('Dose (Gy)')
